7841279572b5ec42336169ca41bb90f926901882
Pablo Velasquez <46729289+pevb97@users.noreply.github.com>
2023-10-09 12:54:39 -0500

    Upload Parte2

---
+++ Parte2/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+dataset = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+filtroDeads = dataset['DEATH_EVENT'] == 1
+deadsDataset = dataset[filtroDeads]
+lifesDataset = dataset[~filtroDeads]
+promAgeDeads = deadsDataset['age'].mean()
+promAgeLifes = lifesDataset['age'].mean()
